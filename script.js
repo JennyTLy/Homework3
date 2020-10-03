e8ffe7e115a05f8c064f5f6dae4a962f9342fe8d
JennyTLy <JennyTLy@gmail.com>
2020-10-03 10:21:19 -0700

    prompting user to select if they'd like to use special characters

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ var generateBtn = document.querySelector("#generate");
 //below is a sequence of prompts that will instruct the user to select options
 var standardChar = "abcdefghijklmnopqrstuvxyz1234567890"
 
-//prompt user to select if they'd like to use uppercase letters in their password
-var upperCaseChoice = prompt("Would you like to include uppercase letters? Please type 'true' for yes or 'false' for no")
-
 
 //prompt to select password length
 var charLength = prompt("For your auto-generated password, please select a lengeth between 8 to 15")
+
+//prompt user to select if they'd like to use uppercase letters in their password
+var upperCaseChoice = prompt("Would you like to include uppercase letters? Please type 'true' for yes or 'false' for no")
+
+//prompt user to select if they'd like to use special characters in their password
+var specialCharChoice = prompt("Would you like to include special characters in your password? Please type 'true' for yes or 'false' for no")
+
 
 // Write password to the #password input
 function writePassword() {
